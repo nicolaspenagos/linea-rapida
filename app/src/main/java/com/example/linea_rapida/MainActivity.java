998fdda4e446ad367912c9876f8ab3ecb329f243
nicolaspenagos <nicolas.penagosm98@gmail.com>
2021-06-05 02:25:49 -0500

    Auth loggin added

--- app/src/main/java/com/example/linea_rapida/MainActivity.java
+++ app/src/main/java/com/example/linea_rapida/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.linea_rapida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() == null){
+            goToLogin();
+        }
+    }
+
+    private void goToLogin() {
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+
     }
 }
