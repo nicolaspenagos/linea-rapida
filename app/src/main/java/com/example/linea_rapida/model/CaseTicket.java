b99a0b1da12d538bc9e8f0f3e51ae6e97242b949
Ana Muñoz <munoz.am.118@gmail.com>
2021-06-14 03:41:53 -0500

    fixed case ticket class conflicts

--- app/src/main/java/com/example/linea_rapida/model/CaseTicket.java
+++ app/src/main/java/com/example/linea_rapida/model/CaseTicket.java
@@ -2,15 +2,12 @@ package com.example.linea_rapida.model;
 
 public class CaseTicket {
 
-<<<<<<< Updated upstream
+
     public static final String STATUS_STARTED = "1";
     public static final String STATUS_IN_PROGRESS = "2";
     public static final String STATUS_FINISHED = "3";
 
-    private long date;
-=======
     private String date;
->>>>>>> Stashed changes
     private String username;
     private String userid;
     private String location;
@@ -18,25 +15,17 @@ public class CaseTicket {
     private String status;
     private String pacientName;
     private long number;
-<<<<<<< Updated upstream
-    private long pacientAge;
-    private String id;
 
-=======
     private String namePatient;
     private String patientAge;
     private String patientId;
->>>>>>> Stashed changes
 
     public CaseTicket() {
     }
 
-<<<<<<< Updated upstream
-    public CaseTicket(long date, String username, String userid, String location, String body, String status, String pacientName, long number, long pacientAge, String id) {
-=======
     public CaseTicket(String body, String date, String patientId, String username, String userid, String location,  String status, long number,
                       String namePatient, String patientAge) {
->>>>>>> Stashed changes
+
         this.date = date;
         this.username = username;
         this.userid = userid;
@@ -45,24 +34,14 @@ public class CaseTicket {
         this.status = status;
         this.pacientName = pacientName;
         this.number = number;
-<<<<<<< Updated upstream
-        this.pacientAge = pacientAge;
-        this.id = id;
-    }
-
-
-    public long getDate() {
-=======
         this.namePatient = namePatient;
         this.patientAge = patientAge;
         this.patientId = patientId;
 
     }
 
-    public String getDate() {
->>>>>>> Stashed changes
-        return date;
-    }
+    public String getDate() { return date;}
+
 
     public void setDate(String date) {
         this.date = date;
@@ -116,30 +95,6 @@ public class CaseTicket {
         this.number = number;
     }
 
-<<<<<<< Updated upstream
-    public String getPacientName() {
-        return pacientName;
-    }
-
-    public void setPacientName(String pacientName) {
-        this.pacientName = pacientName;
-    }
-
-    public long getPacientAge() {
-        return pacientAge;
-    }
-
-    public void setPacientAge(long pacientAge) {
-        this.pacientAge = pacientAge;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-=======
     public String getPatientName() {
         return namePatient;
     }
@@ -162,6 +117,6 @@ public class CaseTicket {
 
     public void setPatientId(String id) {
         this.patientId = id;
->>>>>>> Stashed changes
+
     }
 }
